78f62110aa2d13c635da4362d953b04a5866cb4c
Slava <slavikgassiev1872002@gmail.com>
2024-04-03 18:10:07 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import piece.Seccond;
+
+public class Main {
+    public static void main(String[] args) {
+        Seccond myObj = new Seccond();
+        myObj.hello("HI");
+    }
+}
---
+++ src/Rook.java
@@ -0,0 +1,8 @@
+public class Rook extends Piece{
+
+    public Rook(Color color, Coordinates coordinates) {
+        super(color, coordinates);
+        //TODO Auto-generated constructor stub
+    }
+
+}
