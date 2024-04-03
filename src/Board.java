0c1cc9e0339d8a31a825d1fee30ff46aa4949567
Slava <slavikgassiev1872002@gmail.com>
2024-04-03 19:06:47 +0300

    logik_making

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,17 @@
-public class Board {
+import java.util.HashMap;
 
+public class Board {
+    HashMap<Coordinates, Piece> pieces = new HashMap<Coordinates, Piece>();
+
+    public void setPiece(Coordinates coordinates, Piece piece) {
+        piece.coordinates = coordinates;
+        pieces.put(coordinates, piece);
+    }
+
+    public void setupDefaulPiecesPositions() {
+        for(File file : File.values()) {
+            setPiece(new Coordinates(file, 2), new Pawn(Color.WHITE, new Coordinates(file, 2)));
+            setPiece(new Coordinates(file, 7), new Pawn(Color.WHITE, new Coordinates(file, 7)));
+        }
+    }
 }
