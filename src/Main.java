f041218bf4ba2c03c6b027ccff7e44d7c01dfe60
Slava <slavikgassiev1872002@gmail.com>
2024-04-03 19:56:39 +0300

    z

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 
 public class Main {
     public static void main(String[] args) {
+       Board board = new Board();
+       board.setupDefaulPiecesPositions();
+       int x = 3;
        
     }
 }
